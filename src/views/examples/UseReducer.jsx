67399ad5b0e814129348b6dabbbfbc44a051841b
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-06-24 09:26:21 -0300

    useReducer challenge

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -19,6 +19,22 @@ function reducer(state, action) {
             return {
                 ...state, number: state.number + 2
             }
+        case 'numberMulti7':
+            return {
+                ...state, number: state.number * 7
+            }
+        case 'numberDiv25':
+            return {
+                ...state, number: state.number / 25
+            }
+        case 'numberAddN':
+            return {
+                ...state, number: state.number + action.payload
+            }
+        case 'numberInt':
+            return {
+                ...state, number: parseInt(state.number)
+            }
         case 'login':
             return { ...state, user: { name: action.payload } }
         default:
@@ -49,7 +65,18 @@ const UseReducer = props => {
                     </button>
                     <button className="btn"
                         onClick={() => dispatch({ type: 'numberAdd2' })}>+2</button>
+                    <button className="btn"
+                        onClick={() => dispatch({ type: 'numberMulti7' })}>*7</button>
+                    <button className="btn"
+                        onClick={() => dispatch({ type: 'numberDiv25' })}>/25</button>
+                    <button className="btn"
+                        onClick={() => dispatch({ type: 'numberInt' })}>Transformar em int</button>
+                         <button className="btn"
+                        onClick={() => dispatch({ type: 'numberAddN', payload: -9 })}> -9 </button>
+                         <button className="btn"
+                        onClick={() => dispatch({ type: 'numberAddN', payload: +11 })}> +11 </button>
                 </div>
+
             </div>
         </div>
     )
